113b427d6d87c9548c7450bf8c2d4e21c9bf7f03
abdelrhman_ali <abdelrahman.ali.sayed27@gmail.com>
2020-08-04 08:30:18 +0200

    add mobile design and change functions names

--- js/main.js
+++ js/main.js
@@ -1,17 +1,53 @@
-function myFunction1() {
-  let myDropdown = document.getElementById("mydropdown2");
+function dropLang() {
+  let myDropdown = document.getElementById("drop-cur");
   if (myDropdown.classList.contains('show')) {
     myDropdown.classList.remove('show');
   }
-  document.getElementById("mydropdown1").classList.toggle("show");
+  document.getElementById("drop-lang").classList.toggle("show");
 }
 
-function myFunction2() {
-  let myDropdown = document.getElementById("mydropdown1");
+function dropCur() {
+  let myDropdown = document.getElementById("drop-lang");
   if (myDropdown.classList.contains('show')) {
     myDropdown.classList.remove('show');
   }
-  document.getElementById("mydropdown2").classList.toggle("show");
+  document.getElementById("drop-cur").classList.toggle("show");
+}
+
+function showList() {
+  let list = document.getElementById("header");
+  list.style.display = "block";
+}
+
+function closeList() {
+  let list = document.getElementById("header");
+  list.style.display = "none";
+}
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navMobile").style.backgroundColor = "rgba(00, 00, 00, 0.9)";
+    document.getElementById("navMobile").style.height = "80px";
+    document.getElementById("list").style.height = "80px";
+    document.getElementById("logo").style.fontSize = "30px";
+  } else {
+    document.getElementById("navMobile").style.backgroundColor = "transparent"
+    document.getElementById("navMobile").style.height = "100px";
+    document.getElementById("logo").style.fontSize = "36px";
+  }
+}
+
+window.onscroll = function () { scrollFunction() };
+
+window.onclick = function (e) {
+  if (!e.target.matches('.header')) {
+    let list = document.getElementById("header");
+    if (list.style.display == "block") {
+      list.style.display = "none";
+    }
+  }
 }
 
 window.onclick = function (e) {
@@ -31,3 +67,4 @@ window.onclick = function (e) {
     }
   }
 }
+
